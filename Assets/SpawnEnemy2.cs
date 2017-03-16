c7a7b9e247c08be56b9287d71eca653a6ddd4683
nirmitpatel <nirmitpatel1994@gmail.com>
2017-03-15 22:55:18 -0800

    Level 2 completed

---
+++ Assets/SpawnEnemy2.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class Wave2
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2;
+    public int maxEnemies = 20;
+}
+
+public class SpawnEnemy2 : MonoBehaviour
+{
+
+    public GameObject[] waypoints;
+    public GameObject testEnemyPrefab;
+
+    public Wave2[] waves2;
+    public int timeBetweenWaves = 10;
+
+    private GameManagerBehavior gameManager;
+
+    private float lastSpawnTime;
+    private int enemiesSpawned = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastSpawnTime = Time.time;
+        gameManager =
+            GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1
+        int currentWave = gameManager.Wave;
+        if (currentWave < waves2.Length)
+        {
+            // 2
+            float timeInterval = Time.time - lastSpawnTime;
+            float spawnInterval = waves2[currentWave].spawnInterval;
+            if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) ||
+                 timeInterval > spawnInterval) &&
+                enemiesSpawned < waves2[currentWave].maxEnemies)
+            {
+                // 3  
+                lastSpawnTime = Time.time;
+                GameObject newEnemy = (GameObject)
+                    Instantiate(waves2[currentWave].enemyPrefab);
+                newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints;
+                enemiesSpawned++;
+            }
+            // 4 
+            if (enemiesSpawned == waves2[currentWave].maxEnemies &&
+                GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                gameManager.Wave++;
+                gameManager.Gold = Mathf.RoundToInt(gameManager.Gold * 1.1f);
+                enemiesSpawned = 0;
+                lastSpawnTime = Time.time;
+            }
+            // 5 
+        }
+        else
+        {
+            gameManager.gameOver = true;
+            GameObject gameOverText = GameObject.FindGameObjectWithTag("GameWon");
+            //SceneManager.LoadScene("GameScene2");
+            gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+        }
+    }
+}
